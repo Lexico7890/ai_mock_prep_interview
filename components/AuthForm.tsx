409f12dcc55335b8e597f8689b4d6764bce2804f
Oscar Casas <oscaralexander2626@gmail.com>
2025-04-03 23:05:11 -0500

    change logo

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -78,8 +78,8 @@ const AuthForm = ({ type }: { type: FormType }) => {
     <div className="card-border lg:min-w-[566px]">
       <div className="flex flex-col gap-6 card py-14 px-10">
         <div className="flex flex-row gap-2 justify-center">
-          <Image src="/logo.svg" alt="logo" width={38} height={32} />
-          <h2 className="text-primary-100">PrepWise</h2>
+          <Image src="/logo.png" alt="logo" width={38} height={32} />
+          <h2 className="text-primary-100">AI Job Ninja</h2>
         </div>
         <h3>Practice job interview with AI</h3>
         <Form {...form}>
