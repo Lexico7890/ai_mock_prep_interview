2392ef30edb97a71fe716ca696cd74348d27e547
Oscar Casas <oscaralexander2626@gmail.com>
2025-04-09 13:06:52 -0500

    Se agrega primera parte en dashboard

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,94 +1,81 @@
-import Link from "next/link";
-import Image from "next/image";
+"use client";
 
 import { Button } from "@/components/ui/button";
-import InterviewCard from "@/components/InterviewCard";
+import Image from "next/image";
+import React, { useEffect } from "react";
+import { FaAmazon, FaFacebook, FaGoogle } from "react-icons/fa";
+import { animate, createScope, createSpring } from "animejs";
 
-import { getCurrentUser } from "@/lib/actions/auth.action";
-import {
-  getInterviewsByUserId,
-  getLatestInterviews,
-} from "@/lib/actions/general.action";
-
-async function Home() {
-  const user = await getCurrentUser();
-
-  const [userInterviews, allInterview] = await Promise.all([
-    user?.id ? getInterviewsByUserId(user.id) : [],
-    user?.id ? getLatestInterviews({ userId: user.id }) : [],
-  ]);
-
-  const hasPastInterviews = (userInterviews?.length ?? 0) > 0;
-  const hasUpcomingInterviews = allInterview && allInterview.length > 0;
-
+const Page = () => {
+  useEffect(() => {
+    createScope({}).add(() => {
+      animate(".facebook", {
+        scale: [
+          { to: 1.25, ease: "inOut(3)", duration: 200 },
+          { to: 1, ease: createSpring({ stiffness: 300 }) },
+        ],
+        loop: true,
+        loopDelay: 350,
+      });
+      animate(".google", {
+        scale: [
+          { to: 1.25, ease: "inOut(3)", duration: 200 },
+          { to: 1, ease: createSpring({ stiffness: 300 }) },
+        ],
+        loop: true,
+        loopDelay: 240,
+      });
+      animate(".amazon", {
+        scale: [
+          { to: 1.25, ease: "inOut(3)", duration: 200 },
+          { to: 1, ease: createSpring({ stiffness: 300 }) },
+        ],
+        loop: true,
+        loopDelay: 280,
+      });
+    });
+  }, []);
   return (
-    <>
-      <section className="card-cta">
-        <div className="flex flex-col gap-6 max-w-lg">
-          <h2>Get Interview-Ready with AI-Powered Practice & Feedback</h2>
-          <p className="text-lg">
-            Practice real interview questions & get instant feedback
+    <section className="flex gap-4 py-10">
+      <article className="grid grid-cols-2 gap-4 justify-center items-center">
+        <div className="flex flex-col gap-6 justify-start items-start">
+          <span className="text-base font-semibold text-center bg-orange-custom/30 px-3 py-1 rounded-full text-orange-custom">
+            AI-Powered Interview Practice
+          </span>
+          <p className="text-5xl font-bold">
+            Master Your Interviews with AI Feedback
           </p>
-
-          <Button asChild className="btn-primary max-sm:w-full">
-            <Link href="/interview">Start an Interview</Link>
-          </Button>
+          <p className="text-lg">
+            Practice interviews with our AI interviewer and get real-time
+            feedback to improve your skills and land your dream job.
+          </p>
+          <div className="flex gap-4">
+            <Button>Start Practicing Now</Button>
+            <Button variant="outline">Watch Demo</Button>
+          </div>
         </div>
-
-        <Image
-          src="/robot.png"
-          alt="robo-dude"
-          width={350}
-          height={350}
-          className="max-sm:hidden"
-        />
-      </section>
-
-      <section className="flex flex-col gap-6 mt-8">
-        <h2>Your Interviews</h2>
-
-        <div className="interviews-section">
-          {hasPastInterviews ? (
-            userInterviews?.map((interview) => (
-              <InterviewCard
-                key={interview.id}
-                userId={user?.id}
-                interviewId={interview.id}
-                role={interview.role}
-                type={interview.type}
-                techstack={interview.techstack}
-                createdAt={interview.createdAt}
-              />
-            ))
-          ) : (
-            <p>You haven&apos;t taken any interviews yet</p>
-          )}
+        <div className="flex flex-col items-center relative">
+          <FaFacebook className="facebook absolute top-1/3 right-1/10 h-14 w-14 text-blue-600" />
+          <FaGoogle className="google absolute top-2/3 left-1/12 h-18 w-18 text-green-600" />
+          <FaAmazon className="amazon absolute top-0 left-1/8 h-12 w-12 text-yellow-600"/>
+          <Image
+            src="/robot.png"
+            alt="robo-dude"
+            width={350}
+            height={350}
+            className="max-sm:hidden"
+          />
+          <div className="flex gap-2 items-center mt-4">
+            <span className="relative flex size-3">
+              <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-sky-400 opacity-75"></span>
+              <span className="relative inline-flex size-3 rounded-full bg-sky-500"></span>
+            </span>
+            <p className="relative">AI Interviewer is ready</p>
+          </div>
         </div>
-      </section>
-
-      <section className="flex flex-col gap-6 mt-8">
-        <h2>Take Interviews</h2>
-
-        <div className="interviews-section">
-          {hasUpcomingInterviews ? (
-            allInterview?.map((interview) => (
-              <InterviewCard
-                key={interview.id}
-                userId={user?.id}
-                interviewId={interview.id}
-                role={interview.role}
-                type={interview.type}
-                techstack={interview.techstack}
-                createdAt={interview.createdAt}
-              />
-            ))
-          ) : (
-            <p>There are no interviews available</p>
-          )}
-        </div>
-      </section>
-    </>
+      </article>
+    </section>
   );
-}
+};
 
-export default Home;
+export default Page;
---
+++ components/Header.tsx
@@ -0,0 +1,43 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import Image from "next/image";
+import Link from "next/link";
+import { CircleUserRound } from "lucide-react";
+
+const Header = () => {
+  return (
+    <header className="flex justify-between bg-gray-custom-one border-b-[.5px] border-orange-custom py-3 px-4 items-center fixed top-0 left-0 right-0 z-50">
+      <div className="flex gap-8">
+        <Link href="/" className="flex items-center gap-2">
+          <Image src="/logo.png" alt="logo" width={38} height={32} />
+          <h2 className="text-gray-400 text-base">AI Interview Ninja</h2>
+        </Link>
+        <nav className="flex justify-center items-center">
+          <ul className="flex gap-6">
+            <ol>
+              <Link
+                href="/interviews"
+                className="text-gray-400 hover:border-b-2 hover:border-gray-400"
+              >
+                Interviews
+              </Link>
+            </ol>
+          </ul>
+        </nav>
+      </div>
+      <div>
+        <Link href="/sign-in" className="flex items-center gap-2 font-semibold px-2 py-1 hover:bg-gray-400 hover:text-background rounded-full transition-colors duration-200">
+          <CircleUserRound /> Ingreso
+        </Link>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
+
+/**
+ * <Avatar>
+        <AvatarImage src="https://github.com/shadcn.png" />
+        <AvatarFallback>CN</AvatarFallback>
+      </Avatar>
+ */
